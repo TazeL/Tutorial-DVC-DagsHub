2cbfc44edb58ad8be1c6e9c15cf2f082783bfdf8
Elijah Roussos <elijah.rou@gmail.com>
2022-07-29 14:29:06 +0200

    add preprocess.py and modify train.py

---
+++ preprocess.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+
+# Load the data, sample such that the target classes are equal size
+df = pd.read_csv("data/train_transaction.csv")
+df = pd.concat(
+    [df[df.isFraud == 0].sample(n=len(df[df.isFraud == 1])), df[df.isFraud == 1]],
+    axis=0,
+)
+
+# Select the features and target
+X = df[["ProductCD", "P_emaildomain", "R_emaildomain", "card4", "M1", "M2", "M3"]]
+y = df.isFraud
+
+# Use one-hot encoding to encode the categorical features
+enc = OneHotEncoder(handle_unknown="ignore")
+enc.fit(X)
+
+X = pd.DataFrame(
+    enc.transform(X).toarray(), columns=enc.get_feature_names_out().reshape(-1)
+)
+X["TransactionAmt"] = df[["TransactionAmt"]].to_numpy()
+
+# Split the dataset and train the model
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+X_train.to_csv("data/X_train.csv", index=False)
+X_test.to_csv("data/X_test.csv", index=False)
+y_train.to_csv("data/y_train.csv", index=False)
+y_test.to_csv("data/y_test.csv", index=False)
--- train.py
+++ train.py
@@ -1,34 +1,13 @@
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
 from xgboost import XGBClassifier
+import joblib
+from sklearn.metrics import accuracy_score, roc_auc_score
 
-# Load the data, sample such that the target classes are equal size
-df = pd.read_csv("data/train_transaction.csv")
-df = pd.concat(
-    [df[df.isFraud == 0].sample(n=len(df[df.isFraud == 1])), df[df.isFraud == 1]],
-    axis=0,
-)
+X_train = pd.read_csv("data/X_train.csv")
+X_test = pd.read_csv("data/X_test.csv")
+y_train = pd.read_csv("data/y_train.csv")
+y_test = pd.read_csv("data/y_test.csv")
 
-# Select the features and target
-X = df[["ProductCD", "P_emaildomain", "R_emaildomain", "card4", "M1", "M2", "M3"]]
-y = df.isFraud
-
-# Use one-hot encoding to encode the categorical features
-enc = OneHotEncoder(handle_unknown="ignore")
-enc.fit(X)
-
-X = pd.DataFrame(
-    enc.transform(X).toarray(), columns=enc.get_feature_names_out().reshape(-1)
-)
-X["TransactionAmt"] = df[["TransactionAmt"]].to_numpy()
-
-# Split the dataset and train the model
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
 xgb = XGBClassifier(
     n_estimators=100,
     learning_rate=0.1,
@@ -43,3 +22,11 @@ xgb = XGBClassifier(
     seed=27,
 )
 model = xgb.fit(X_train, y_train)
+y_pred = xgb.predict(X_test)
+
+accuracy = round(accuracy_score(y_test, y_pred), 3)
+roc_auc = round(roc_auc_score(y_test, y_pred), 3)
+print(f"Accuracy: {accuracy}")
+print(f"ROC AUC: {roc_auc}")
+
+joblib.dump(model, "models/xgb-fraud-classifier.joblib")

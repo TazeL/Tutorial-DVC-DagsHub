e8691f3acd18979b75ae6a1704ddb459ddcc9711
Elijah Roussos <elijah.rou@gmail.com>
2022-08-01 10:16:01 +0200

    update for new version

--- preprocess.py
+++ preprocess.py
@@ -29,7 +29,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-X_train.to_csv("data/X_train.csv", index=False)
-X_test.to_csv("data/X_test.csv", index=False)
-y_train.to_csv("data/y_train.csv", index=False)
-y_test.to_csv("data/y_test.csv", index=False)
+X_train.to_csv("feature_sets/X_train.csv", index=False)
+X_test.to_csv("feature_sets/X_test.csv", index=False)
+y_train.to_csv("feature_sets/y_train.csv", index=False)
+y_test.to_csv("feature_sets/y_test.csv", index=False)
--- train.py
+++ train.py
@@ -2,30 +2,41 @@ import pandas as pd
 from xgboost import XGBClassifier
 import joblib
 from sklearn.metrics import accuracy_score, roc_auc_score
+import dagshub
 
-X_train = pd.read_csv("data/X_train.csv")
-X_test = pd.read_csv("data/X_test.csv")
-y_train = pd.read_csv("data/y_train.csv")
-y_test = pd.read_csv("data/y_test.csv")
+X_train = pd.read_csv("feature_sets/X_train.csv")
+X_test = pd.read_csv("feature_sets/X_test.csv")
+y_train = pd.read_csv("feature_sets/y_train.csv")
+y_test = pd.read_csv("feature_sets/y_test.csv")
 
-xgb = XGBClassifier(
-    n_estimators=100,
-    learning_rate=0.1,
-    max_depth=3,
-    min_child_weight=1,
-    gamma=0,
-    subsample=0.8,
-    colsample_bytree=0.8,
-    objective="binary:logistic",
-    nthread=4,
-    scale_pos_weight=1,
-    seed=27,
-)
-model = xgb.fit(X_train, y_train)
-y_pred = xgb.predict(X_test)
+with dagshub.dagshub_logger() as logger:
+    xgb = XGBClassifier(
+        n_estimators=100,
+        learning_rate=0.1,
+        max_depth=3,
+        min_child_weight=1,
+        gamma=0,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        objective="binary:logistic",
+        nthread=4,
+        scale_pos_weight=1,
+        seed=27,
+    )
+    logger.log_hyperparams(model_class=type(xgb).__name__)
+    logger.log_hyperparams({'model': xgb.get_params()})
 
-accuracy = round(accuracy_score(y_test, y_pred), 3)
-roc_auc = round(roc_auc_score(y_test, y_pred), 3)
+    model = xgb.fit(X_train, y_train)
+    y_pred = xgb.predict(X_test)
+
+    accuracy = round(accuracy_score(y_test, y_pred), 3)
+    roc_auc = round(roc_auc_score(y_test, y_pred), 3)
+    logger.log_metrics(
+        {'accuracy': accuracy}
+    )
+    logger.log_metrics(
+        {'roc_auc': roc_auc}
+    )
 print(f"Accuracy: {accuracy}")
 print(f"ROC AUC: {roc_auc}")
 
